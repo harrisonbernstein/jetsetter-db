12ba512bf216b04f77da55ea4278649603a6c826
Harrison Bernstein <harrisonbernstein@Harrisons-MacBook-Pro.local>
2023-11-30 18:20:26 -0500

    more bookings endpoint

--- flask-app/src/bookings/bookings.py
+++ flask-app/src/bookings/bookings.py
@@ -8,6 +8,10 @@ bookings = Blueprint('bookings', __name__)
 @bookings.route('/bookings', methods=['GET'])
 def get_bookings():
     cursor = db.get_db().cursor()
-    cursor.execute('select * from bookings')
-    bookings = cursor.fetchall()
-    return jsonify(bookings)
+    cursor.execute('select * from Booking')
+    json_data = []
+    column_headers = [x[0] for x in cursor.description]
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))   
+    return jsonify(json_data)
