dc706ce52f0249ee30677f54a7845bcfd2244cb7
Harrison Bernstein <harrisonbernstein@Harrisons-MacBook-Pro.local>
2023-12-02 15:07:33 -0500

    bookings endpoint work

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -37,6 +37,7 @@ def create_app():
     # from src.products.products  import products
     from src.passengers.passengers import passengers
     from src.airports.airports import airports
+    from src.bookings.bookings import bookings
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
@@ -44,6 +45,7 @@ def create_app():
     # app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(passengers)
     app.register_blueprint(airports, url_prefix='/a')
+    app.register_blueprint(bookings, url_prefix='/b')
 
     # Don't forget to return the app object
     return app
