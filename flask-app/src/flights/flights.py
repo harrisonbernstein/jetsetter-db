195e6b4a9aa576c40f47149a899c491c7ac9242b
Simar Chadha <simarchadha1@gmail.com>
2023-12-04 21:38:38 -0500

    More passenger routes and finished flight routes

--- flask-app/src/flights/flights.py
+++ flask-app/src/flights/flights.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 import json
 from src import db
+from datetime import datetime
 
 flights = Blueprint('flights', __name__)
 
@@ -8,15 +9,20 @@ flights = Blueprint('flights', __name__)
 def get_flights():
     if request.method == 'POST':
         data = request.json
-        flightID = data['flightID']
-        departure = data['departure']
-        arrival = data['arrival']
-        departureTime = data['departureTime']
-        arrivalTime = data['arrivalTime']
-        aircraftID = data['aircraftID']
+        aircraft = data['aircraft']
+        destinationAirport = data['destinationAirport']
+        destinationGate = data['destinationGate']
+        duration = datetime.strptime(data['duration'], '%H:%M:%S').time()
+        flightId = data['flightId']
+        landingTime = datetime.strptime(data['landingTime'], '%Y-%m-%d %H:%M:%S')
+        managerID = data['managerID']
+        originAirport = data['originAirport']
+        originGate = data['originGate']
+        status = data['status']
+        takeOffTime = datetime.strptime(data['takeOffTime'], '%Y-%m-%d %H:%M:%S')
         cursor = db.get_db().cursor()
-        cursor.execute('insert into Flight (flightID, departure, arrival, departureTime, arrivalTime, aircraftID) values (%s, %s, %s, %s, %s, %s)', 
-                    (flightID, departure, arrival, departureTime, arrivalTime, aircraftID))
+        cursor.execute('insert into Flight (aircraft, destinationAirport, destinationGate, duration, flightId, landingTime, managerID, originAirport, originGate, status, takeOffTime) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', 
+                        (aircraft, destinationAirport, destinationGate, duration, flightId, landingTime, managerID, originAirport, originGate, status, takeOffTime,))
         db.get_db().commit()
         return 'Success!'
     if request.method == 'GET':
@@ -48,15 +54,20 @@ def get_flight(flightID):
     return the_response
   if request.method == 'PUT':
     data = request.json
-    flightID = data['flightID']
-    departure = data['departure']
-    arrival = data['arrival']
-    departureTime = data['departureTime']
-    arrivalTime = data['arrivalTime']
-    aircraftID = data['aircraftID']
+    aircraft = data['aircraft']
+    destinationAirport = data['destinationAirport']
+    destinationGate = data['destinationGate']
+    duration = datetime.strptime(data['duration'], '%H:%M:%S').time()
+    flightId = data['flightId']
+    landingTime = datetime.strptime(data['landingTime'], '%Y-%m-%d %H:%M:%S')
+    managerID = data['managerID']
+    originAirport = data['originAirport']
+    originGate = data['originGate']
+    status = data['status']
+    takeOffTime = datetime.strptime(data['takeOffTime'], '%Y-%m-%d %H:%M:%S')
     cursor = db.get_db().cursor()
-    cursor.execute('UPDATE Flight SET departure = %s, arrival = %s, departureTime = %s, arrivalTime = %s, aircraftID = %s WHERE flightID = %s', 
-                    (departure, arrival, departureTime, arrivalTime, aircraftID, flightID))
+    cursor.execute('UPDATE Flight SET aircraft = %s, destinationAirport = %s, destinationGate = %s, duration = %s, landingTime = %s, managerID = %s, originAirport = %s, originGate = %s, status = %s, takeOffTime = %s WHERE flightId = %s', 
+                    (aircraft, destinationAirport, destinationGate, duration, landingTime, managerID, originAirport, originGate, status, takeOffTime, flightId))
     db.get_db().commit()
     return 'Success!'
   if request.method == 'DELETE':
@@ -65,8 +76,9 @@ def get_flight(flightID):
     db.get_db().commit()
     return 'Success!'
 
-@flights.route('/flights/<flightID>/aircraft', methods=['GET'])
+@flights.route('/flights/<flightID>/aircraft', methods=['GET', 'PUT'])
 def get_flight_aircraft(flightID):
+  if request.method == 'GET':
     cursor = db.get_db().cursor()
     cursor.execute('SELECT * FROM Aircraft WHERE aircraftId = (SELECT aircraft from Flight WHERE flightId = %s)', flightID) 
     row_headers = [x[0] for x in cursor.description]
@@ -78,3 +90,12 @@ def get_flight_aircraft(flightID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+  if request.method == 'PUT':
+    data = request.json
+    aircraftID = data['aircraftID']
+    capacity = data['capacity']
+    model = data['model']
+    cursor = db.get_db().cursor()
+    cursor.execute('UPDATE Aircraft SET capacity = %s, model = %s WHERE aircraftID = %s', (capacity, model, aircraftID))
+    db.get_db().commit()
+    return 'Success!'
